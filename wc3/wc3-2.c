8a95e7e1b2159e7d34f9a378d79e0e00783b1e4b
LeeSangHyo <gilsport1@gmail.com>
2020-04-28 15:16:28 +0900

    make Foders

--- wc3/wc3-2.c
+++ wc3/wc3-2.c
@@ -5,7 +5,7 @@ int main(int argc, char * argv[])
 {
 	int first_pid = fork();
 	if(first_pid ==0 ) {
-		for(int i=0;i<10000;i++)
+		for(int i=0;i<100000;i++)
 		printf("A");
 		printf("\n");
 		return 0;
@@ -14,7 +14,7 @@ int main(int argc, char * argv[])
 	int second_pid = fork();
 	if(second_pid == 0 ){
 		
-		for(int i=0;i<10000;i++)
+		for(int i=0;i<100000;i++)
 		printf("B");
 
 		printf("\n");
@@ -22,6 +22,7 @@ int main(int argc, char * argv[])
 		}
 		
 	else {
+		for(int i=0;i<100000;i++)
 		printf("p");
 
 		printf("\n");
